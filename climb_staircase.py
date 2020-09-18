005a64afde2ec482702e596da99d14bf7bef299b
klcysn <klcysn@users.noreply.github.com>
2020-09-18 08:14:43 +0200

    climb stairs by 1 or 2 steps

---
+++ climb_staircase.py
@@ -0,0 +1,14 @@
+# There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+# For example, if N is 4, then there are 5 unique ways:
+# 1, 1, 1, 1
+# 2, 1, 1
+# 1, 2, 1
+# 1, 1, 2
+# 2, 2
+
+n = int(input("How many steps are there ? : "))
+
+def steps(n):
+    return n if n <= 3 else (steps(n-1) + steps(n-2))
+
+steps(n)
